a779535acaaaa4bad6ec9249a86228a456a3de50
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-06-10 10:00:38 +0200

    step2 passed

--- server/app/routers/api/tiles/router.js
+++ server/app/routers/api/tiles/router.js
@@ -3,18 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
 
-const boatsRouter = require("./boats/router");
-const tilesRouter = require("./tiles/router")
+const { browse } = require("../../../controllers/tileActions");
 
-router.use("/boats", boatsRouter);
-router.use("/tiles", tilesRouter)
-
-const gamesRouter = require("./games/router");
-
-router.use("/games", gamesRouter);
+router.get("/", browse);
 
 /* ************************************************************************* */
 
