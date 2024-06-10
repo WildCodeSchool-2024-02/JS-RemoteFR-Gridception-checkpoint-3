2d43d5d7be3503e91bada2f91f02ec3c622b3df2
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-10 12:50:26 +0200

    first version step4...

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -29,8 +29,3 @@ module.exports = {
   edit,
 };
 
-// npm run test step3 : créer une route PUT /api/boats/:id pour mettre à jour un bateau de la base de données.
-// Ta route devra renvoyer un statut 204 si la mise à jour a réussi.
-// Dans ton controller, tu peux accéder avec le paramètre req à :
-// l'id du bateau (req.params.id),
-// ses coordonnées X et Y (req.body.coord_x et req.body.coord_y).
--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,23 @@
-module.exports = null;
+const tileExists = (req, res, next) => {
+    const { coordX, coordY} = req.body;
+    if ( coordX < 0 || coordX >= 11 || coordY < 0 || coordY >= 5){
+        next();
+    } else {
+        res.sendStatus(422)
+    }
+}
+
+
+module.exports = tileExists;
+
+
+
+// npm run test step4 : créer un middleware dans server/app/services/tileExists.js 
+// pour tester si une tuile avec les coordonnées req.body.coord_x et req.body.coord_y 
+// existe ou non dans la base de données.
+// Pour cette étape, idéalement tu devrais utiliser tables.tile pour trouver des tuiles
+// à partir de leurs coordonnées (tu devras ajouter une nouvelle méthode readByCoordinates à TileRepository.js).
+// Si tu as des difficultés à utiliser TileRepository, tu peux t'en passer en 
+// vérifiant que la coordonnée X est comprise entre 0 et 11 (inclus), 
+// et que la coordonnée Y est comprise entre 0 et 5 (inclus).
+// Si les coordonnées sont valides, passe au suivant. Sinon, répond avec un statut 422.
