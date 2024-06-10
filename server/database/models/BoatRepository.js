188c1d08ffb19d855db655bbf30cc75738615485
Benoît Vandanjon <benoit@vandanjon.fr>
2024-06-10 15:26:18 +0200

    bonus AND CHECKPOINT finished

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -5,16 +5,28 @@ class BoatRepository extends AbstractRepository {
     super({ table: "boat" });
   }
 
-  async readAll() {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`
-    SELECT boat.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure
-    FROM ${this.table}
-    JOIN tile
-    ON boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y
-    `);
+  async readAll(where) {
+    if (where == null) {
+      const [rows] = await this.database.query(`
+  SELECT boat.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure
+  FROM ${this.table}
+  JOIN tile
+  ON boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y
+  `);
 
-    // Return the array of boats
+      return rows;
+    }
+
+    const [rows] = await this.database.query(
+      `
+  SELECT boat.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure
+  FROM ${this.table}
+  JOIN tile
+  ON boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y
+  WHERE name = ?
+  `,
+      [where.name]
+    );
     return rows;
   }
 
