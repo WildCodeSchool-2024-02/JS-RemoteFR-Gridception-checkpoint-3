5f53df3091f5e76875788f9d970e80cd44ec0ab7
NicoGER1 <nico3g@hotmail.fr>
2024-06-10 12:51:29 +0200

    step5_test done

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -8,7 +8,23 @@ class BoatRepository extends AbstractRepository {
   // The Rs of CRUD - Read operations
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const [rows] = await this.database.query(`
+          SELECT 
+            boat.id,
+             boat.coord_x,
+              boat.coord_y,
+               boat.name,
+                tile.type,
+                 tile.has_treasure
+          FROM 
+            ${this.table} 
+          INNER JOIN 
+          tile
+          ON
+          boat.coord_x = tile.coord_x 
+          AND
+          boat.coord_y = tile.coord_y 
+        `);
 
     // Return the array of boats
     return rows;
