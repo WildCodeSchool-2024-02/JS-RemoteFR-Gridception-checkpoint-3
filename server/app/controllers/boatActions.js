0473c0b1e5f3ef364848f028e974ba1ce6fe0952
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-10 11:58:46 +0200

    Finish step3

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,24 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  const boats = { ...req.body, id: req.params.id };
+
+  try {
+    await tables.boat.update(boats);
+    res.sendStatus(204);
+  } catch (err){
+    next(err);
+   }
+};
+
 module.exports = {
   browse,
+  edit,
 };
+
+// npm run test step3 : créer une route PUT /api/boats/:id pour mettre à jour un bateau de la base de données.
+// Ta route devra renvoyer un statut 204 si la mise à jour a réussi.
+// Dans ton controller, tu peux accéder avec le paramètre req à :
+// l'id du bateau (req.params.id),
+// ses coordonnées X et Y (req.body.coord_x et req.body.coord_y).
