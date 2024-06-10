65ac177f368801bf33bb21abdb4d3cb6d2fb927f
Benoît Vandanjon <benoit@vandanjon.fr>
2024-06-10 15:08:08 +0200

    step4 finished

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,20 @@
-module.exports = null;
+const tables = require("../../database/tables");
+
+const tileExists = async (req, res, next) => {
+  try {
+    const tiles = await tables.tile.readByCoordinates(
+      req.body.coord_x,
+      req.body.coord_y
+    );
+
+    if (tiles.length !== 0) {
+      next();
+    } else {
+      res.sendStatus(422);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = tileExists;
