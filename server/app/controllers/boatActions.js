89d53edfc5ebc17619f460ea33112bc129603ced
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-06-10 10:20:15 +0200

    step3 tests done

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,19 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  // Extract the boats data from the request body and params
+  const boats = { ...req.body, id: req.params.id };
+
+  try {
+    await tables.boat.update(boats);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   browse,
+  edit,
 };
