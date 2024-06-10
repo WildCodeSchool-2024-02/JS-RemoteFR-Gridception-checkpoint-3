c8e6d480b951d2caf58ea3126eb225f3e9271b45
BonnevieAmel <amel.bonnevie@gmail.com>
2024-06-10 11:11:14 +0200

    test Step3 OK

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -2,11 +2,24 @@ const tables = require("../../database/tables");
 
 const browse = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const boats = await tables.boat.readAll();
 
-    // Respond with the boats in JSON format
     res.json(boats);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const edit = async (req, res, next) => {
+  // Extract the category data from the request body and params
+  const boat = { ...req.body, id: req.params.id };
+
+  try {
+    // Update the category in the database
+    await tables.boat.update(boat);
+
+    // Respond with HTTP 204 (No Content)
+    res.sendStatus(204);
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -15,4 +28,5 @@ const browse = async (req, res, next) => {
 
 module.exports = {
   browse,
+  edit,
 };
