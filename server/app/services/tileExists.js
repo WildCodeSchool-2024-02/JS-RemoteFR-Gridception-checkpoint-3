ffc5feb440feeb39d38a5329bbcb4709b4481938
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-10 15:25:04 +0200

    Correction step 4 fin et step 5

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,4 +1,7 @@
-const tileExists = (req, res, next) => {
+const tables = require("../../database/tables");
+
+/*
+const tileExists = async (req, res, next) => {
     const { coordX, coordY} = req.body;
     if ( coordX < 0 || coordX >= 11 || coordY < 0 || coordY >= 5){
         next();
@@ -6,7 +9,26 @@ const tileExists = (req, res, next) => {
         res.sendStatus(422)
     }
     next();
-}
+} */
 
 
-module.exports = tileExists;
+// CORRECTION EN VERSION DYNAMIQUE
+
+const tileExists = async (req, res, next) => {
+    try {
+      const tiles = await tables.tile.readByCoordinates(
+        req.body.coord_x,
+        req.body.coord_y
+      );
+  
+      if (tiles.length !== 0) {
+        next();
+      } else {
+        res.sendStatus(422);
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
+    
+    module.exports = tileExists;
--- server/database/models/TileRepository.js
+++ server/database/models/TileRepository.js
@@ -14,36 +14,38 @@ class TileRepository extends AbstractRepository {
     // Return the array of tiles
     return rows;
   }
-
-  async readByCoordinates(coordX, coordY) {
-    const [rows] = await this.database.query(
-      `select * from  ${this.table} where coord_x = ? and coord_y = ?`,
-      [coordX, coordY]
-    );
-
-    return rows;
-  }
-
+  
+  
   async getRandomIsland() {
     const [rows] = await this.database.query(
       `select id from ${this.table} where type="island" order by rand() limit 1`
-    );
-
-    return rows[0];
-  }
-
-  async hideTreasure(island) {
-    const [result] = await this.database.query(
-      `update ${this.table} set has_treasure =
+      );
+      
+      return rows[0];
+    }
+    
+    async hideTreasure(island) {
+      const [result] = await this.database.query(
+        `update ${this.table} set has_treasure =
         case
-          when id = ? then true
-          else false
+        when id = ? then true
+        else false
         end`,
-      [island.id]
-    );
-
-    return result;
-  }
-}
-
-module.exports = TileRepository;
+        [island.id]
+        );
+        
+        return result;
+      }
+      
+      async readByCoordinates(coordX, coordY) {
+        const [rows] = await this.database.query(
+          `select * from  ${this.table} where coord_x = ? and coord_y = ?`,
+          [coordX, coordY]
+          );
+          
+          return rows;
+        }
+    }
+    
+    module.exports = TileRepository;
+    
