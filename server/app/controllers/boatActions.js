7be8e146fe8550f957b78fcdb21c0b286ed2eed4
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-06-10 11:19:15 +0200

    step 3 passed

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,28 @@ const browse = async (req, res, next) => {
   }
 };
 
+const read = async (req, res, next) => {
+  try {
+    const boat = await tables.boat.read(req.params.id);
+    res.json(boat);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const edit = async (req, res, next) => {
+  const boat = { ...req.body, id: req.params.id };
+
+  try {
+    await tables.boat.update(boat);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err)
+  }
+}
+
 module.exports = {
   browse,
+  read,
+  edit,
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -12,6 +12,16 @@ class BoatRepository extends AbstractRepository {
     // Return the array of boats
     return rows;
   }
+
+  async read(id) {
+    const [rows] = await this.database.query(`select * from ${this.table} WHERE id = ?`, [id]);
+    return rows[0];
+  }
+
+  async update(boat) {
+    const [result] = await this.database.query(`update ${this.table} set coord_x=?, coord_y=? where id=?`, [boat.coord_x, boat.coord_y, boat.id]);
+    return result.affectedRows;
+  }
 }
 
 module.exports = BoatRepository;
