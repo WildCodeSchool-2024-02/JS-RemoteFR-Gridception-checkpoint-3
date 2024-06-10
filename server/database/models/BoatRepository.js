598c4fe31f104d979268b65d20534f76de06c697
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-06-10 13:24:05 +0200

    Finish step 5

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -7,7 +7,22 @@ class BoatRepository extends AbstractRepository {
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const [rows] = await this.database.query(
+      `
+       SELECT
+                boat.id,
+                boat.name,
+                boat.coord_x,
+                boat.coord_y,
+                tile.id as tile_id,
+                tile.type,
+                tile.has_treasure
+            FROM 
+                ${this.table}
+            LEFT JOIN 
+                tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y
+      `
+    );
 
     // Return the array of boats
     return rows;
