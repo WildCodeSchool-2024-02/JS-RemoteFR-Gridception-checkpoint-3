423e2df39472cef7be201176e7dce198a30eb64b
BonnevieAmel <amel.bonnevie@gmail.com>
2024-06-10 13:20:27 +0200

    test Step5 OK

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -6,11 +6,28 @@ class BoatRepository extends AbstractRepository {
   }
 
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    try {
+      // Execute the SQL SELECT query to retrieve all boats from the "boat" table
+      const [rows] = await this.database.query(`
+            SELECT
+                boat.id,
+                boat.name,
+                boat.coord_x,
+                boat.coord_y,
+                tile.id as tile_id,
+                tile.type,
+                tile.has_treasure
+            FROM 
+                ${this.table} AS boat
+            LEFT JOIN 
+                tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y
+        `);
 
-    // Return the array of boats
-    return rows;
+      // Return the array of boats with tile information
+      return rows;
+    } catch (error) {
+      throw new Error(`Error in BoatRepository.readAll: ${error.message}`);
+    }
   }
 
   async update(boat) {
