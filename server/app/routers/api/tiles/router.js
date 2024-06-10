28f511a79b87fd7e128958ba72c3cf349ab7abf5
Angélina Droz <angieso91@gmail.com>
2024-06-10 10:44:40 +0200

    finish step 2

---
+++ server/app/routers/api/tiles/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { browse, } = require("../../../controllers/tileActions");
+
+router.get("/", browse);
+
+
+module.exports = router;
